3b2bb7314dd942a297ce5a1c3af9d1a0a8f9458f
Binto Joseph <bintojoseph543@gmail.com>
2022-10-21 01:14:16 +0530

    main page added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:youtube_clone/presentation/main_page/screen_main_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,11 +10,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Youtube_clone',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Text('Home'),
+      home: const ScreenMainPage(),
     );
   }
 }
