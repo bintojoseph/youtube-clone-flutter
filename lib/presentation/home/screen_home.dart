42d07ebc00360a0c2564339d7056c6e62e82c07f
Binto Joseph <bintojoseph543@gmail.com>
2022-10-23 01:22:12 +0530

    homelist widget updated

--- lib/presentation/home/screen_home.dart
+++ lib/presentation/home/screen_home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:youtube_clone/core/constants.dart';
 import 'package:youtube_clone/presentation/widgets/app_bar_widget.dart';
 
 class ScreenHome extends StatelessWidget {
@@ -8,8 +9,93 @@ class ScreenHome extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar:  PreferredSize(preferredSize: Size.fromHeight(50),child: AppbarWidget(),),
+    return Scaffold(
+      appBar: const PreferredSize(
+        preferredSize: Size.fromHeight(50),
+        child: AppbarWidget(),
+      ),
+      body: ListView.separated(
+        itemBuilder: (BuildContext context, int index) {
+          return const HomeListWidget();
+        },
+        separatorBuilder: (BuildContext context, int index) {
+          return const SizedBox(
+            height: 5,
+          );
+        },
+        itemCount: 10,
+      ),
+    );
+  }
+}
+
+class HomeListWidget extends StatelessWidget {
+  const HomeListWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 295,
+      width: double.infinity,
+      child: Column(
+        children: [
+          Container(
+            height: 222,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(
+                    'https://img.youtube.com/vi/abLD7p0xSYk/maxresdefault.jpg'),
+              ),
+            ),
+          ),
+          Container(
+            height: 73,
+            color: const Color.fromRGBO(46, 44, 44, 1),
+            child: Row(
+              children: [
+                kWidth,
+                Container(
+                  height: 40,
+                  width: 40,
+                  decoration: const BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.all(Radius.circular(30)),
+                  ),
+                  child: const Icon(
+                    Icons.person_rounded,
+                    size: 30,
+                  ),
+                ),
+                kWidth,
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    SizedBox(
+                      width: 300,
+                      child: Text(
+                        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s',
+                        overflow: TextOverflow.clip,
+                        maxLines: 2,
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                    Text("Moto GP . 300K views . 2 months ago",style: TextStyle(color: Colors.grey,),),
+                  ],
+                ),
+                const Icon(
+                  Icons.more_vert,
+                  color: Colors.white,
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
